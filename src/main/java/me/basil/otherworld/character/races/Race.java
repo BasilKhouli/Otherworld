07188182e0ec95359e58849550b3ca7c26a5d235
BasilKhouli <basil.khouli@gmail.com>
2026-02-05 04:44:22 +1300

    4am things

--- src/main/java/me/basil/otherworld/character/races/Race.java
+++ src/main/java/me/basil/otherworld/character/races/Race.java
@@ -1,8 +1,44 @@
 package me.basil.otherworld.character.races;
+import com.hypixel.hytale.server.core.modules.entitystats.modifier.Modifier;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public abstract class Race {
+import static me.basil.otherworld.character.races.AbilityManager.registerAbility;
 
+public abstract class Race {
+    public final String name;
+    private final Map<String,Ability> abilities = new HashMap<>();
+    protected final Map<String, Modifier> modifiers;
+
+    public Race(String raceName, List<Ability> raceAbilities, Map<String, Modifier> raceModifiers) {
+        this.name = raceName;
+
+        for (Ability ability : raceAbilities) {
+            registerAbility(ability);
+        }
+        modifiers = raceModifiers;
+    }
+
+    abstract public Race clone();
+
+    public void registerAbility(Ability ability) {
+        String abilityName = null;
+        abilityName = ability.name.toLowerCase();
+        abilities.put(abilityName, ability);
+    }
+
+    pubic Ability getAbility(String abilityName) {
+        abilityName = abilityName.toLowerCase();
+        if (!(abilities.containsKey(abilityName))) {
+            return null;
+        }
+        return abilities.get(abilityName);
+    }
+
+    public Map<String, Ability> getAbilities() {
+        return abilities;
+    }
 
 }
--- src/main/java/me/basil/otherworld/character/races/RaceManager.java
+++ src/main/java/me/basil/otherworld/character/races/RaceManager.java
@@ -1,6 +1,12 @@
 package me.basil.otherworld.character.races;
 
 
+import me.basil.otherworld.character.races.fiend.Fiend;
+import me.basil.otherworld.character.races.merefolk.Merefolk;
+import me.basil.otherworld.character.races.spiritborn.Spiritborn;
+import me.basil.otherworld.character.races.vampire.Vampire;
+import me.basil.otherworld.character.races.werewolf.Werewolf;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +16,21 @@ public class RaceManager {
 
     public static void registerRace(Race race){
         races.put(races.name,race);
+    }
+
+    public static Ability getAbility(Ability ability, String abilityName) {
+        return ability.getAbility(abilityName);
+    }
+
+    publc static void initialize(){
+
+        registerRace(new Vampire());
+        registerRace(new Werewolf());
+        registerRace(new Fiend());
+        registerRace(new Merefolk());
+        registerRace(new Spiritborn());
+
+
     }
 
 
