f0478524905bb7f21fed64773f3a70b3042d0999
BasilKhouli <basil.khouli@gmail.com>
2026-02-10 12:18:27 +1300

    Assigned PlayerReadyEvent to WorldThread

--- src/main/java/me/basil/otherworld/Main.java
+++ src/main/java/me/basil/otherworld/Main.java
@@ -3,10 +3,12 @@ package me.basil.otherworld;
 import com.hypixel.hytale.component.*;
 import com.hypixel.hytale.event.EventRegistry;
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import me.basil.otherworld.character.races.RaceManager;
 import me.basil.otherworld.commands.OtherWorldDataCommand;
@@ -34,25 +36,29 @@ public class Main extends JavaPlugin {
         this.getCommandRegistry().registerCommand(new OtherWorldDataCommand());
 
         ComponentRegistryProxy<EntityStore> eSR =this.getEntityStoreRegistry();
-        EventRegistry er = this.getEventRegistry();
         OWDcomponentType = eSR.registerComponent(OtherworldData.class,"OtherworldData",OtherworldData.CODEC);
 
         eSR.registerSystem(new RaceSystem());
 
-        er.registerGlobal(PlayerReadyEvent.class, this::onPlayerReady);
 
         RaceManager.initialize();
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, (event) -> {
+            Player player = event.getPlayer();
+            World world = player.getWorld();
+            world.execute(() -> {
+
+                Ref<EntityStore> ref = event.getPlayerRef();
+                Store<EntityStore> store = ref.getStore();
+                PlayerRef playerRef = store.getComponent(ref,PlayerRef.getComponentType());
+                assert playerRef != null;
+                OtherworldData owData = store.ensureAndGetComponent(ref, OtherworldData.getComponentType());
+                //only temp till we add way to choose
+                owData.chooseRace("Vampire",playerRef);
+
+            });
+
+        });
+
     }
 
-    //Events (Could be moved to a diff class to prevent clutter or whatever)
-    private void onPlayerReady(PlayerReadyEvent event){
-        Ref<EntityStore> ref = event.getPlayerRef();
-        Store<EntityStore> store = ref.getStore();
-        PlayerRef playerRef = store.getComponent(ref,PlayerRef.getComponentType());
-        assert playerRef != null;
-        OtherworldData owData = store.ensureAndGetComponent(ref, OtherworldData.getComponentType());
-        //only temp till we add way to choose
-        owData.chooseRace("Vampire",playerRef);
-
-    }
 }
